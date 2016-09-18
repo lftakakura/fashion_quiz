51668827630872fb7db0b2fa428c88c20b37264a
Elton Morais <moraisesc@gmail.com>
2016-09-18 18:38:08 -0300

    Added gender and age to Lead

--- lead/models.py
+++ lead/models.py
@@ -5,12 +5,19 @@ from django.db import models
 
 
 class Lead(models.Model):
+    GENDER_CHOICES = [
+        (0, 'Masculino'),
+        (1, 'Feminino')
+    ]
+
     name = models.CharField('nome', max_length=128)
     email = models.EmailField('e-mail', max_length=256, unique=True)
-    token = models.UUIDField(default=uuid.uuid4, unique=True)
+    gender = models.PositiveIntegerField('sexo', choices=GENDER_CHOICES, null=True)
+    age = models.PositiveIntegerField('idade', null=True)
+    accepted_partners = models.BooleanField(default=True)
     ip_address = models.GenericIPAddressField(protocol='both', unpack_ipv4=True)
     created_on = models.DateTimeField(default=datetime.now)
-    accepted_partners = models.BooleanField(default=True)
+    token = models.UUIDField(default=uuid.uuid4, unique=True)
 
     def __str__(self):
         return '{} ({})'.format(self.name, self.email)
