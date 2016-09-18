7fe694da68371cb3418c6041465073f0f6434337
Elton Morais <moraisesc@gmail.com>
2016-09-18 13:53:45 -0300

    Changed way as create lead

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^new-lead$', views.save_lead, name='save_lead'),
+    url(r'^(?P<token>[0-9a-z-]+)$', views.index, name='index')
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,19 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from lead.models import Lead
 
 
-def index(request):
+def save_lead(request):
     if request.POST:
         name = request.POST.get('name')
+        token = request.POST.get('token')
+        accepted_partners = True
 
-        Lead.objects.create(
+        if request.POST.get('accept-partners') is None:
+            accepted_partners = False
+
+        lead = Lead.objects.create(
             name=name,
             email=request.POST.get('email'),
-            token=request.POST.get('token'),
-            ip_address=request.POST.get('ip')
+            token=token,
+            ip_address=request.POST.get('ip'),
+            accepted_partners=accepted_partners
         )
 
+        lead.save()
+
+        return redirect(lead.get_absolute_url(), permanent=True)
+
+
+def index(request, token):
+    lead = get_object_or_404(Lead, token=token)
+
     return render(request, 'quiz/index.html', {
-        'name': name,
+        'name': lead.name
     })
