f9112f7514140d495be7f975b44d7901f525e2b7
Elton Morais <moraisesc@gmail.com>
2016-09-19 10:06:12 -0300

    Added ALLOWED_HOSTS

--- fashion_quiz/settings.py
+++ fashion_quiz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '&me(5%1e=16t$0j1f)eoxi-q_vqrv$=ent7es5v5wekq%0pil@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'quizdoestilo']
 
 
 # Application definition
